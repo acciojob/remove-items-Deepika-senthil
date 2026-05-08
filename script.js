d620d070a46b82e2d72a81d4a692e5e72a6640b1
Deepika-senthil <deepikasutherson0706@gmail.com>
2026-05-07 23:14:59 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let selectColor document.getElementById("colorSelect");
+function removecolor(){
+		if(selectColor.selectedIndex !== -1)
+		{
+			selectColor.remove(selectColor.selectedIndex);
+		}
+	}
